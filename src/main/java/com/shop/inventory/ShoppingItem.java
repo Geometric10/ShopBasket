0222bde09864a40b52cb62251936311f6a1d553e
Geometric10 <sandeepreddya79@gmail.com>
2017-04-27 21:25:09 +0530

    Commiting ShopBasket Maven Project

---
+++ src/main/java/com/shop/inventory/ShoppingItem.java
@@ -0,0 +1,23 @@
+package com.shop.inventory;
+
+import java.math.BigDecimal;
+
+/**
+ * Sandeep
+ * Represents a item which can be shopped
+ */
+public interface ShoppingItem {
+
+    /**
+     * Get the name of the item. This also acts as the key for now
+     * @return the name of the item,which is also the key
+     */
+    String getName();
+
+    /**
+     * return the selling price of the item
+     * @return the selling price of the item
+     */
+    BigDecimal getPrice();
+
+}
